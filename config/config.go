d8df578ee0bc6b5548307328f2456db5682465ad
khafiluhur <khafiluhur.99@gmail.com>
2025-10-30 10:20:36 +0700

    hotfix(config): hide promo credential

--- config/config.go
+++ config/config.go
@@ -31,7 +31,7 @@ type Config struct {
 	ServiceTimeout int    `envconfig:"SERVICE_TIMEOUT" default:"30"`
 	ExpiredService int    `envconfig:"EXPIRED_SERVICE" default:"3600"`
 	UserService    string `envconfig:"USER_SERVICE" default:"https://sandbox-api.tripdeals.id"`
-	PlatformConfig string `envconfig:"PROMO_CREDENTIAL" default:"W3sicGxhdGZvcm0iOiJ0cmlwZGVhbHMiLCJzeXN0ZW0iOiJ0cmlwZGVhbHMiLCJzZWNyZXQiOiJhMjgyMmIwNi02ODc0LTQ1ODktOWZmZS00MDdhMTdkOTI5MTEifSx7InBsYXRmb3JtIjoidHJpcGRlYWxzY21zIiwic3lzdGVtIjoidHJpcGRlYWxzIiwic2VjcmV0IjoiMTJkMmNjZTAtODQ1Yi00YTQyLThhYzEtNTlkZWRiNzM5ZGZmIn0seyJwbGF0Zm9ybSI6ImdvbGRlbnJhbWEiLCJzeXN0ZW0iOiJnb2xkZW5yYW1hIiwic2VjcmV0IjoiYTg5OGE4YjktNmQ1My00ODI5LWEzN2EtYmQzOTBmMmQxMzA5In1d"`
+	PlatformConfig string `envconfig:"PROMO_CREDENTIAL" default:""`
 }
 
 var data *Config
